06767cdcdafc4280c94bd9750a887f66be4bb8a6
durgeshahire07 <durgeshahire07@gmail.com>
2024-03-05 22:49:33 +0530

    fix: :package: react-native-randombytes+1.0.0.patch

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
+import './shim'
 
 const App = () => {
   return (
--- global.js
+++ global.js
@@ -1,5 +1,7 @@
 import {Buffer} from 'buffer';
 import 'react-native-get-random-values';
+import { randomBytes } from 'crypto';
 
 global.crypto.getRandomValues;
 global.Buffer = Buffer;
+global.randomBytes = randomBytes;
