4f136d55468761e01ea5b5d083250a39fda06568
durgeshahire07 <durgeshahire07@gmail.com>
2024-03-10 22:13:42 +0530

    feat: :rocket: added boilerplate for bitcoinjs-lib & ethers

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import Routes from "./src/routes";
+import { View, Text } from "react-native";
 import './shim'
+import ECPairFactory from 'ecpair';
+import * as ecc from '@bitcoin-js/tiny-secp256k1-asmjs'
+import * as Bitcoin from 'bitcoinjs-lib';
+import { ethers } from 'ethers';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginHorizontal: 30 }}>
+      <Text>Wohoo!!</Text>
+      <Text style={{ textAlign: 'center' }}>You have successfully added bitcoinjs-lib, ethers and other polyfills into your react native app</Text>
+    </View>
   );
 };
 
